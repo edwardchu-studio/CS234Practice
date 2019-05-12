62c11b60d963bdc2656c530378b53adc3aaf2ccf
edwardchor <czy8023@me.com>
2019-05-12 16:56:51 -0400

    init

---
+++ modules/Environment.py
@@ -0,0 +1,10 @@
+class Environment:
+    def __init__(self):
+        pass
+    def reset(self):
+        pass
+    def step(self,action):
+        pass
+
+if __name__ == '__main__':
+    E=Environment()
